af32e683aa27086697b35d32bdd380555a56e446
rose <rosed.20104@gmail.com>
2026-06-23 10:55:09 -0400

    TRYING to make the testing of api work
    this is probably gonna take a while bc I'm still learning js

--- api.js
+++ api.js
@@ -1,3 +1,6 @@
+// geting api info
+getInfo()
+
 let day = "1";
 let month = "1"
 
@@ -10,9 +13,18 @@ async function getInfo(){
     }
     const data = await response.json();
     console.group(data);
+
+    const url = "https://today.zenquotes.io/api/" + month + "/" + day
+     //https://today.zenquotes.io/api/1/1 example
+    const answer = document.getElementById("answerbox")
   }
   catch (error) {
   console.log(error.message);
 
 
+}
+}
+//getting info from button? 
+function sendInfo() {
+  const userInput = 
 }
