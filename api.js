ff6286f77dca4d77b88e827b69198dc3a974fefe
rose <rosed.20104@gmail.com>
2026-06-24 19:51:29 -0400

    I found a new api to use (wikipedia) so I replaced the code to make it fit with this api and I noticed it had genres of events that it was sorted by so I also added code that let the user sort by that

--- api.js
+++ api.js
@@ -2,31 +2,36 @@
 function saveInfo() {
   const monthValue = document.getElementById("month")
   const dayValue = document.getElementById("days")
+  const genreValue = document.getElementById("genre")
   const button = document.getElementById("button")
 
   button.addEventListener("click", async () => {
     month = monthValue .value;
     day = dayValue.value;
+    genre = genreValue.value;
     
-    await getInfo(month, day);
+    await getInfo(month, day, genre);
   })
 }
 
 // geting api info 
-async function getInfo(userMonth, userDay){
-  const url = `https://today.zenquotes.io/api/${userMonth}/${userDay}`;
-  //https://today.zenquotes.io/api/1/1 example
-  const answer = document.getElementById("answerbox");
+async function getInfo(userMonth, userDay, userGenre){
+  const url = `https://byabbe.se/on-this-day/${userMonth}/${userDay}/${userGenre}.json`;
+  //https://byabbe.se/{month}/{day}/events.json example
+  const answer = document.getElementById("answertext");
 
   try {
+    answer.innerText = "Loading! Please wait...if nothing shows after 10 secs, your date is invalid. Please choose a new date if so.";
     const response = await fetch(url);
     if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
+      throw new Error('Response status: ${response.status}');
     }
     const data = await response.json();
     console.log(data);
+    const randomYear = Math.floor(Math.random() * (data.year).length);
+
   }
   catch (error) {
-  console.log("error");
+    console.log("error", error);
   }
 }
