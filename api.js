bce28743454642babe30c9c5e9470d506bf3044b
rose <rosed.20104@gmail.com>
2026-06-24 21:02:48 -0400

    i got the api to show up on the actual webpage!!

--- api.js
+++ api.js
@@ -6,7 +6,7 @@ function saveInfo() {
   const button = document.getElementById("button")
 
   button.addEventListener("click", async () => {
-    month = monthValue .value;
+    month = monthValue.value;
     day = dayValue.value;
     genre = genreValue.value;
     
@@ -14,21 +14,35 @@ function saveInfo() {
   })
 }
 
-// geting api info 
+// getting api info 
 async function getInfo(userMonth, userDay, userGenre){
   const url = `https://byabbe.se/on-this-day/${userMonth}/${userDay}/${userGenre}.json`;
   //https://byabbe.se/{month}/{day}/events.json example
   const answer = document.getElementById("answertext");
 
   try {
-    answer.innerText = "Loading! Please wait...if nothing shows after 10 secs, your date is invalid. Please choose a new date if so.";
+    answer.innerText = "Loading! If nothing shows after 10 seconds, your date is invalid. Please choose a new date if so.";
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error('Response status: ${response.status}');
     }
     const data = await response.json();
     console.log(data);
-    const randomYear = Math.floor(Math.random() * (data.year).length);
+
+    const allYears = data[userGenre]
+    const randomYearNumber = Math.floor(Math.random() * allYears.length);
+    const findYearInfo = allYears[randomYearNumber];
+    const trueRandomYear = findYearInfo.year;
+    const answerText = findYearInfo.description || findYearInfo.name;
+    if (userGenre == "events") {
+       answer.innerText = `In ${trueRandomYear}, ${answerText}`;
+    }
+    else if (userGenre == "births") {
+       answer.innerText = `In ${trueRandomYear}, ${answerText}, was born.`;
+    }
+    else if (userGenre == "deaths") {
+       answer.innerText = `In ${trueRandomYear}, ${answerText}, died.`;
+    }
 
   }
   catch (error) {
